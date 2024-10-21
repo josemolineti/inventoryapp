e466b2f7358091cc3a43cb3c91812fe8f9981285
VJMPASSOS <141372133+VJMPASSOS@users.noreply.github.com>
2024-10-21 00:48:40 -0300

    Fiz quase nada em muito tempo
    eu fiz aquilo q te mostrei, mas daí percebi q tava errado, então resolvi fazer de novo

--- src/components/ui/top-bar.tsx
+++ src/components/ui/top-bar.tsx
@@ -1,21 +1,86 @@
-import React from "react";
-import Button from "./button";
-import { JSXSource } from "react/jsx-dev-runtime";
+import { useEffect, useState } from "react";
 import Logo from "./logo";
+import "@/styles/componentsStyles.css";
+import Close from "@/assets/close.svg";
+import Menu from "@/assets/menu.svg";
 
-function  TopBar():JSX.Element{
-    return(
-<nav className='top-bar'>
-            <Logo clickable={true} onlyLogo={true}/>
-            <div className='top-bar-secondy-part'>
-                <ul className='top-bar-ul'>
-                    <Button text='Home' color={4} link="/home"/>
-                    <Button text='Fornecedores' color={4} link="/fornecedores" />
-                    <Button text='Produtos' color={4} link="/produtos"/>
-                    <Button text='Clientes' color={4} link="/clientes"/>
-                    <Button text='nada ainda' color={4} />
-                    <Button text='Sair' color={4} link="/login"/>
+function TopBar(): JSX.Element {
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 720);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 720);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+
+    const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+    return (
+        <nav className='top-bar'>
+            <div className="logobar">
+                <Logo clickable={true} onlyLogo={!isMobile} />
+            </div>
+            <div className='desktop'>
+                <ul>
+                    <li>
+                        <a href="/home">Home</a>
+                    </li>
+                    <li>
+                        <a href="/fornecedores">Fornecedores</a>
+                    </li>
+                    <li>
+                        <a href="/produtos">Produtos</a>
+                    </li>
+                    <li>
+                        <a href="/clientes">Clientes</a>
+                    </li>
+                    <li>
+                        <a href="/#">Nada</a>
+                    </li>
+                    <li>
+                        <a href="/login">Sair</a>
+                    </li>
                 </ul>
+            </div>
+            <div className="mobile-bg">
+                {showMobileMenu ?
+                    <div className="mobile">
+                        <ul>
+                            <li>
+                                <a href="/home">Home</a>
+                            </li>
+                            <li>
+                                <a href="/fornecedores">Fornecedores</a>
+                            </li>
+                            <li>
+                                <a href="/produtos">Produtos</a>
+                            </li>
+                            <li>
+                                <a href="/clientes">Clientes</a>
+                            </li>
+                            <li>
+                                <a href="/#">Nada</a>
+                            </li>
+                            <li>
+                                <a href="/login">Sair</a>
+                            </li>
+                        </ul>
+                        <span onClick={() => setShowMobileMenu(!showMobileMenu)} className="btn-wrapper">
+                            <img src={Close} alt="ícone fechar menu" width={24} height={24} />
+                        </span>
+                    </div>
+                    :
+                        <span onClick={() => setShowMobileMenu(!showMobileMenu)} className="btn-wrapper" >
+                            <img src={Menu} alt="ícone menu" width={24} height={24} />
+                        </span>
+                }
             </div>
         </nav>
 
